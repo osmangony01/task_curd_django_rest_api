7ef5a5e8bdc7f18782c808e9b5bf4422bd7a8b51
Md Osman Gony <mdosmangoni376@gmail.com>
2025-05-10 21:54:45 +0600

    implement update task api

--- taskCurdApi/views.py
+++ taskCurdApi/views.py
@@ -26,7 +26,17 @@ class TaskView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    
+    def put(self, request, pk):
+        try:
+            task = Task.objects.get(pk=pk)
+        except Task.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
 
